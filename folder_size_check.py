372e89eb2ff514920065015f3b0b2e5718a4dbfa
sibaru <coolshinji@163.com>
2014-04-26 13:49:00 +0800

    Signed-off-by: sibaru <coolshinji@163.com>

--- folder_size_check.py
+++ folder_size_check.py
@@ -1,17 +1,24 @@
 import os
 import os.path
+import sys
 from os.path import join, getsize, isfile
 
-target_dir = r'C:\Program Files (x86)\Common Files'	
-for f in os.listdir(target_dir):
-	if f[0] == r'.':
-		continue
-	if os.path.isfile(join(target_dir,f)):
-		print(join(target_dir,f),':',getsize(join(target_dir,f))/1024/1024,'M')
-	if os.path.isdir(join(target_dir,f)):
-		filesize = 0
-		for root, dirs, files in os.walk(join(target_dir,f)):
-			for name in files:
-				filesize+=getsize(join(root,name))
-		print(join(target_dir,f),':',filesize/1024/1024,'M')
-
+if len(sys.argv)<=1:
+	print('请于调用时输入目标文件夹名')
+	exit(-1)
+for index in range(1,len(sys.argv)):
+	target_dir = sys.argv[index]
+	print('***********************************')
+	print('文件夹'+target_dir+'使用情况统计')
+	for f in os.listdir(target_dir):
+		if f[0] == r'.':
+			continue
+		if os.path.isfile(join(target_dir,f)):
+			print(join(target_dir,f),':',getsize(join(target_dir,f))/1024/1024,'M')
+		if os.path.isdir(join(target_dir,f)):
+			filesize = 0
+			for root, dirs, files in os.walk(join(target_dir,f)):
+				for name in files:
+					filesize+=getsize(join(root,name))
+			print(join(target_dir,f),':',filesize/1024/1024,'M')
+print('***********************************')	
